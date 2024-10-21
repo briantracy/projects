593746e5c79c8dad63cb2f0110dfde8944d950dc
Brian C Tracy <brian.tracy33@gmail.com>
2024-10-20 19:17:13 -0700

    data is flowing

--- systemb/brp/client.cpp
+++ systemb/brp/client.cpp
@@ -10,60 +10,27 @@
 #include <condition_variable>
 #include <cstdint>
 #include <iostream>
+#include <limits>
 #include <mutex>
 #include <queue>
 #include <stdexcept>
 #include <string_view>
 #include <thread>
 
-struct Packet {
-  static constexpr int PayloadMaxBytes = 20;
-  static constexpr int PacketMinBytes = 1;
-  static constexpr int PacketMaxBytes = PacketMinBytes + PayloadMaxBytes;
-  // These occupy the first 3 bits of the packet
-  enum Flags : uint8_t {
-    Hello = 0b00,
-    Goodbye = 0b01,
-    Data = 0b10,
-    Heartbeat = 0b11,
-    // Ack can modify any of the above types.
-    Ack = 0b100,
-  };
-
-  std::array<uint8_t, PayloadMaxBytes> payload;
-  uint8_t flags;
-  uint8_t sequence_number;
-  uint8_t payload_length;
-
-  std::string to_string() const {
-    return "Packet{type=" + std::to_string(flags & ~Flags::Ack) +
-           ",ack=" + std::to_string(!!(flags & Flags::Ack)) +
-           ",seq=" + std::to_string(sequence_number) +
-           ",len=" + std::to_string(payload_length) + "}";
-  }
-};
-
-constexpr uint8_t PacketFlagMask = 0b00000011;
-enum PacketFlags {
-  Data = 0b00,
-  Ack = 0b01,
-};
-
-
 class BRPClient {
   // Calls recv in a loop
   std::thread listen_thread;
-  // Sleep with these while waiting for an ack
-  std::mutex ack_mutex;
+  // Sleep with these while waiting for an ack.
+  // Also use mutex to synchronize access to read buffer
+  std::mutex mutex;
   std::condition_variable ack_cv;
 
-  int sequence_number_sent;
-  int ack_number_received;
+  int sequence_number_sent = 0;
+  int ack_number_received = -1;
 
-  // also need read seqno so we know what to expect.
+  std::vector<uint8_t> read_buffer;
 
   int sockfd;
-  bool connected;
 public:
 
   BRPClient(const char *ip, short port, bool is_server);
@@ -72,10 +39,13 @@ public:
   int read(char *buff, int length);
 
  private:
+  static constexpr uint8_t AckFlag = 0b1;
+  static constexpr uint8_t DataFlag = 0b10;
+  static constexpr int PacketMaxSize = 10;
   void listen_thread_func();
 };
 
-BRPClient::BRPClient(const char *ip, short port, bool is_server) : connected(!is_server) {
+BRPClient::BRPClient(const char *ip, short port, bool is_server) {
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd == -1) {
     throw std::runtime_error("could not create socket");
@@ -120,28 +90,38 @@ BRPClient::~BRPClient() {
 using std::uint8_t;
 
 int BRPClient::write(const char *data, int length) {
-  assert(connected);
-  constexpr int packet_size = 10;
-  for (int i = 0; i < length; i+= packet_size) {
+  constexpr int PayloadMaxSize = PacketMaxSize - 2;
+  for (int i = 0; i < length; i+= PayloadMaxSize) {
     for (int tries = 0; tries < 3; ++tries) {
-      char packet[packet_size + 1];
-      packet[0] = 0;
-      memcpy(packet + 1, data, length);
-      send(sockfd, packet, length + 1, 0);
-      std::unique_lock<std::mutex> lock(ack_mutex);
+      char packet[PacketMaxSize];
+      packet[0] = DataFlag;
+      packet[1] = sequence_number_sent;
+      const int payload_bytes = std::min(PayloadMaxSize, length - i);
+      memcpy(packet + 2, data + i, payload_bytes);
+      send(sockfd, packet, payload_bytes + 2, 0);
+      std::unique_lock<std::mutex> lock(mutex);
       ack_cv.wait_for(lock, std::chrono::seconds(1));
       if (ack_number_received == sequence_number_sent) {
         break;
       }
     }
+    ++sequence_number_sent;
   }
+  return 0;
 }
-int BRPClient::read(char *buff, int length) { return 0; }
+
+int BRPClient::read(char *buff, int length) {
+  std::unique_lock<std::mutex> lock(mutex);
+  const int bytes_read = std::min(length, (int)read_buffer.size());
+  std::memcpy(buff, read_buffer.data(), bytes_read);
+  read_buffer.erase(read_buffer.begin(), read_buffer.begin() + bytes_read);
+  return bytes_read;
+}
 
 
 void BRPClient::listen_thread_func() {
   std::cout << "listen thread\n";
-
+  bool connected = false;
   while (true) {
     char buff[1 << 16];
     sockaddr_in sender;
@@ -153,6 +133,9 @@ void BRPClient::listen_thread_func() {
       throw std::runtime_error{"recvfrom error: " +
                                std::string(std::strerror(errno))};
     }
+    if (bytes_read < 2) {
+      throw std::runtime_error("packet too small");
+    }
     if (!connected) {
       int r = connect(sockfd, (sockaddr *)&sender, socklen);
       assert(r == 0);
@@ -160,27 +143,34 @@ void BRPClient::listen_thread_func() {
     }
     printf("got packet of size: %zd!\n", bytes_read);
 
-    if ((buff[0] & PacketFlagMask) == PacketFlags::Data) {
+    if (buff[0] == DataFlag) {
       printf("got data");
       if (bytes_read < 2) {
         printf("malformed packet -- too small\n");
         break;
       }
       const uint8_t seqno = static_cast<uint8_t>(buff[1]);
-      printf("remote seqno = %d\n", seqno);
-      const uint8_t ack_packet[2] = {PacketFlags::Ack, seqno};
+      printf(" -- remote seqno = %d\n", seqno);
+      const uint8_t ack_packet[2] = {AckFlag, seqno};
       int r = send(sockfd, ack_packet, sizeof(ack_packet), 0);
       if (r == -1) {
         perror("sendto");
       }
+      // TODO: duplicate packet detection
+      std::unique_lock<std::mutex> lock(mutex);
+      std::copy(&buff[2], &buff[bytes_read - 2], std::back_inserter(read_buffer));
     }
 
-    if ((buff[0] & PacketFlagMask) == PacketFlags::Ack) {
+    if (buff[0] == AckFlag) {
       printf("got ack");
       if (bytes_read < 2) {
         printf("malformed packet -- too small\n");
         break;
       }
+      const uint8_t ack_seqno = buff[1];
+      std::unique_lock<std::mutex> lock(mutex);
+      ack_number_received = ack_seqno;
+      ack_cv.notify_one();
     }
 
   }
@@ -190,7 +180,9 @@ int main(const int argc, const char *const argv[]) {
   BRPClient client("127.0.0.1", 9989, false);
   BRPClient server("127.0.0.1", 9989, true);
 
-  client.write("qqq", 3);
-  char buff[3];
-  server.read(buff, 3);
+  const char* text = "This is some text being sent from one client to another";
+  client.write(text, strlen(text));
+  char buff[10] = {0};
+  server.read(buff, 9);
+  printf("we got from client: %s\n", buff);
 }
